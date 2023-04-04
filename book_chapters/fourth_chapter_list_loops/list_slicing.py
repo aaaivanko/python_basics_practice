44a489b7e033e16c426db7806b51a05045a81b83
Ivan <ivan.chvartatskyy@gmail.com>
2023-04-04 23:22:08 +0200

    done 4 chapter loop lists and tuples

---
+++ book_chapters/fourth_chapter_list_loops/list_slicing.py
@@ -0,0 +1,5 @@
+# slicing
+
+some_list = ['here', 'we', 'go', 'to', 'you']
+
+print(some_list[1:3])
---
+++ book_chapters/fourth_chapter_list_loops/pizzas.py
@@ -0,0 +1,16 @@
+# pizzas
+
+pizzas = ['paperonni', 'cheese', 'onion']
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza!")
+print(f"I really love pizza!")
+
+
+# animals
+
+animals = ['dog', 'cat', 'tiger', 'chicken']
+
+for animal in animals:
+    print(f"I would like to have {animal} as a pet")
+    
