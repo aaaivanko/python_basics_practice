cd638c96350eda46650479f454dd9bc1f1573c44
Ivan <ivan.chvartatskyy@gmail.com>
2023-04-04 19:31:40 +0200

    done 3 chapter lists simple examples

---
+++ book_chapters/third_chapter/names.py
@@ -0,0 +1,14 @@
+# names
+names = ['Bob', 'Charley', 'David', 'Eric']
+
+for name in names:
+    print(f"Hello, {name}")
+
+
+# cars
+
+cars = ['Honda', 'BMW', 'Mazda']
+
+for car in cars:
+    print(car[1])
+    
