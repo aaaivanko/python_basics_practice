38b0d0d9f13642d3bbb7bda7ac6f8e93f36c1afe
Ivan <ivan.chvartatskyy@gmail.com>
2023-04-05 15:28:35 +0200

    done 5 part of book about if else elif

---
+++ book_chapters/fifth_if_chapter/condition.py
@@ -0,0 +1,38 @@
+# condition
+# car = 'bmw'.upper()
+#
+# if car == 'bmw'.upper():
+#     print(True)
+# else:
+#     print(False)
+
+# alien colours
+# alien_color = 'black'
+#
+# points = 0
+#
+# if alien_color == 'green':
+#     points += 5
+# elif alien_color == 'red':
+#     points += 10
+# elif alien_color == 'yellow':
+#     points += 10
+#
+# print(f"{alien_color} got {points} points")
+
+# live phases
+
+age = 4
+
+if age <= 2:
+    print('offspring')
+elif age <= 4:
+    print('little kid')
+elif age <= 12:
+    print('little adult')
+elif age <= 20:
+    print('adult')
+elif age >= 60:
+    print('old men')
+
+
